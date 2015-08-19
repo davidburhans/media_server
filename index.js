9dc0574b7d11ea735b34b3f671c9f6ed0868e327
David Burhans <david.burhans@gmail.com>
2015-08-18 20:24:02 -0600

    avoiding callback hell

--- index.js
+++ index.js
@@ -20,105 +20,165 @@ var options = {
 };
 
 function endsWith(str, ends) {
-    return str.indexOf(ends) == (str.length - ends.length);
+  return str.indexOf(ends) == (str.length - ends.length);
 }
 
-http.createServer(options, function (req, res) {
-  if(req.method == 'POST') {
-    var d = '';
-    req.on('data', function(data) {
-      d += data;
-    });
-    req.on('end', function() {
-      console.log('POST', d);
-      var isMagnet = d.match(/magnet:\?xt=urn:[\:a-z0-9]{20,50}/i) != null;
-      res.writeHead(isMagnet ? 200 : 500);
-      if(isMagnet) {
-          var proc = child_process.spawn('deluge-console', ['add', d]);
-          var response = '';
-          proc.stdout.on('data', function(resData) {
-              response += resData;
-          });
-          proc.on('close', function(status) {
-          	res.end(response);
-          });
-      } else {
-          res.end();
-      }
-    });
+function delugePostMiddleware(req, res, cb) {
+  if(!req.body) {
+    cb();
     return;
   }
-  var relPath = path.join('.', qs.unescape(req.url.replace('..', '')));
-  if(endsWith(relPath, '.ico')) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end();
-    return;
-  }
-  console.log('relPath is', relPath, endsWith(relPath, defaultExt));
-  if (!endsWith(relPath, defaultExt)) {
-    var mediaDir = path.join(mediaPath, relPath);
-    console.log('listing', mediaDir);
-    fs.stat(mediaDir, function(err, stats) {
-        if(err) {
-            res.end(err.message);
-            return;
-        }
-        if(stats.isDirectory()) {
-            fs.readdir(mediaDir, function(err, files) {                
-                if(err) {
-                    res.end(err.message);
-                    return;
-                }     
-                res.writeHead(200, { "Content-Type": "text/html" });
-                for(var c = 0; c < files.length; ++c) {
-                    var href = path.join(req.url, files[c]);
-                    if(blacklist.indexOf(href) > -1) {
-                        continue;
-                    }
-                    try {
-                        var subStat = fs.statSync(path.join(mediaDir, files[c]));
-                        if(subStat.isFile() && endsWith(href, defaultExt)) {
-                            res.write('<video src="' + href + '" controls></video>');
-                        } else if (subStat.isDirectory()) {
-                            res.write('<a href="' + href + '">' + href + '</a><br />');
-                        }
-                    } catch(ex) {
-                        console.log('Exception', ex);
-                    }
-                }
-                res.end();
-            });
-        }
-    });
-  } else {
-    var file = path.join(mediaPath, relPath);
-    console.log('streaming', file);
-    var range = req.headers.range;
-    var positions = range.replace(/bytes=/, "").split("-");
-    var start = parseInt(positions[0], 10);
-    console.log('reading', range);
-    fs.stat(file, function(err, stats) {
-      var total = stats.size;
-      var max = start + maxBlockSize;
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
-      if(end > max) {
-	end = max;
-      }
-      var chunksize = (end - start) + 1;
-
-      res.writeHead(206, {
-        "Content-Range": "bytes " + start + "-" + end + "/" + total,
-        "Accept-Ranges": "bytes",
-        "Content-Length": chunksize,
-        "Content-Type": "video/mp4"
+  var isMagnet = req.body.match(/magnet:\?xt=urn:[\:a-z0-9]{20,50}/i) != null;
+  res.writeHead(isMagnet ? 200 : 500);
+  if(isMagnet) {
+      var proc = child_process.spawn('deluge-console', ['add', req.body]);
+      var response = '';
+      proc.stdout.on('data', function(resData) {
+          response += resData;
       });
-
-      var stream = fs.createReadStream(file, { start: start, end: end })
-        .on("open", function() {
-          stream.pipe(res);
-        }).on("error", function(err) {
-          res.end(err);
-        });
-    });
+      proc.on('close', function(status) {
+        res.write(response);
+        cb();
+      });
+  } else {
+      cb();
   }
-}).listen(8888);
+}
+
+function extractBody(req, res, cb) {
+  var body = '',
+      cancel = false;
+  req.on('data', function(data) {
+    body += data;
+    if(body.length > 2048) {
+      cancel = true;
+      req.end();
+      res.end();
+    }
+  });
+  req.on('end', function() {
+    if(cancel) {
+      cb('cancelled');
+      return;
+    }
+    if(body) {
+      console.log('BODY', body);
+      req.body = body;
+    }
+    cb();
+  });
+}
+
+function ignoreIco(req, res, cb) {
+  if(endsWith(req.url, '.ico')) {
+      res.writeHead(200);
+      res.end();
+  } else {
+    cb();
+  }
+}
+
+function renderFileListing(req, res) {
+  var files = req.mediaFiles,
+      mediaDir = req.mediaDir;
+  for(var c = 0; c < files.length; ++c) {
+    var href = path.join(mediaDir, files[c]).replace(mediaPath, '');
+    if(blacklist.indexOf(href) > -1) {
+        continue;
+    }
+    try {
+        var subStat = fs.statSync(path.join(mediaDir, files[c]));
+        if(subStat.isFile() && endsWith(href, defaultExt)) {
+            res.write('<video src="' + href + '" controls></video>');
+        } else if (subStat.isDirectory()) {
+            res.write('<a href="' + href + '">' + href + '</a><br />');
+        }
+    } catch(ex) {
+        console.log('Exception', ex);
+    }
+  }
+}
+
+function streamMediaFile(req, res, cb) {
+  var file = req.mediaDir;
+  console.log('streaming', file);
+  var range = req.headers.range;
+  var positions = range.replace(/bytes=/, "").split("-");
+  var start = parseInt(positions[0], 10);
+  console.log('reading', range, file);
+  fs.stat(file, function(err, stats) {
+    if (err) {
+       cb(err.message);
+       return;
+    }
+    var total = stats.size;
+    var max = start + maxBlockSize;
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+    if(end > max) {
+      end = max;
+    }
+    var chunksize = (end - start) + 1;
+
+    res.writeHead(206, {
+      "Content-Range": "bytes " + start + "-" + end + "/" + total,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4"
+    });
+
+    var stream = fs.createReadStream(file, { start: start, end: end })
+    stream.on("open", function() {
+      stream.pipe(res);
+    });
+    stream.on('end', cb);
+    stream.on("error", cb);
+  });
+}
+
+function mediaListing(req, res, cb) {
+    var relPath = path.join('.', qs.unescape(req.url).replace('..', ''));
+    req.mediaDir = path.join(mediaPath, relPath);
+    console.log('listing', req.mediaDir, relPath);
+    if (!endsWith(req.mediaDir, defaultExt)) {
+      fs.stat(req.mediaDir, function(err, stats) {
+          if(err) {
+              cb(err.message);
+              return;
+          }
+          if(stats.isDirectory()) {
+            fs.readdir(req.mediaDir, function(err, files) {
+              if(err) {
+                  cb(err.message);
+                  return;
+              }
+              req.mediaFiles = files;
+              res.writeHead(200, { "Content-Type": "text/html" });
+              renderFileListing(req, res);
+              cb();
+            });
+          }
+      });
+    } else {
+      streamMediaFile(req, res, cb);
+  }
+}
+
+function wrapRequest(req, res) {
+  var middleware = [ignoreIco, extractBody, delugePostMiddleware, mediaListing];
+  function middlewareCallback(idx, err) {
+    if(err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end(err);
+      return;
+    }
+    if(idx >= middleware.length) {
+      res.end();
+      return;
+    }
+    console.log('calling middleware', idx);
+    middleware[idx](req, res, middlewareCallback.bind(null, idx+1));
+  }
+  middlewareCallback(0);
+}
+
+http.createServer(options, wrapRequest).listen(8888);
